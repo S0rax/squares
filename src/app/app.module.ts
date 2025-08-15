b99398910dace0b42232c4076fcc9be29748694a
S0rax <s0rax@dmginc.gg>
2025-08-15 16:48:33 +0200

    ng core update to 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        HttpClientModule,
-        FontAwesomeModule
-    ],
-    providers: [],
+    declarations: [AppComponent],
     bootstrap: [AppComponent],
+    imports: [BrowserModule, BrowserAnimationsModule, FontAwesomeModule],
+    providers: [provideHttpClient(withInterceptorsFromDi())]
 })
 export class AppModule {}
